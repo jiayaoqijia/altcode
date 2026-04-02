70db55f56f832c751bacbe8068eeb0ce2c279c02
jiayaoqijia <jiayaoqijia@users.noreply.github.com>
2026-04-02 17:02:44 +0000

    fix: clean model-generated files (again)

--- internal/max.go
+++
@@ -1,8 +0,0 @@
-import "cmp"
-
-func Max[T cmp.Ordered](a, b T) T {
-	if a > b {
-		return a
-	}
-	return b
-}
--- internal/user.go
+++
@@ -1,5 +0,0 @@
-func GetUser(db *sql.DB, name string) error {
-	q := "SELECT * FROM users WHERE name=?"
-	_, err := db.Exec(q, name)
-	return err
-}
